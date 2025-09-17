9e3600cdc9eda0a1ac424db4c28cab3c0f5daa74
yao6iu <yao.liu@bsu.edu>
2025-09-16 22:08:55 -0400

    memory

---
+++ memory/memory/memory_game.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var selected_cards : Array
+var cards_match = false
+
+func _ready() -> void:
+	selected_cards = []
+
+func card_clicked(selected_card):
+
+	selected_cards.append(selected_card)
+	if selected_cards.size() >= 2:
+		$FlipTimer.start()
+		
+		if selected_cards[0].card_value == selected_cards[1].card_value:
+			cards_match = true
+		else:
+			cards_match = false
+
+func _on_flip_timer_timeout() -> void:
+	if cards_match:
+		selected_cards[0].queue_free()
+		selected_cards[1].queue_free()
+	else:
+		selected_cards[0].flip()
+		selected_cards[1].flip()
+		
+	selected_cards = []
+	pass # Replace with function body.
