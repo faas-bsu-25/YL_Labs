e6e6292df84eb0a93f843600233c941ac802fd7f
yao6iu <yao.liu@bsu.edu>
2025-11-11 22:43:59 -0500

    ball

---
+++ ball-chase/ball_chase/script/selectable.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@onready var main_mesh = $MeshInstance3D 
+
+func ray_select():
+	main_mesh.get_active_material(0).next_pass.set_shader_parameter("outline_width",10)
+	pass
+
+func ray_deselect():
+	main_mesh.get_active_material(0).next_pass.set_shader_parameter("outline_width",0)
+	pass
+	
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
